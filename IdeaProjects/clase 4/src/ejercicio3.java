8abbe7d5caba83b282669b4ffbc11f4ac373bea7
MD-2117 <danielivancasas@hotmail.com>
2023-03-11 18:05:55 -0300

    update ejercicio 3
    ultimo update, correccion de clase 4 ejercicio 3

--- IdeaProjects/clase 4/src/ejercicio3.java
+++ IdeaProjects/clase 4/src/ejercicio3.java
@@ -1,93 +1,122 @@
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Locale;
-import java.util.Scanner;
+//
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+//
 public class ejercicio3 {
-    static void codificacion(String frase,Path ruta) throws IOException{
-        Files.write(ruta, frase.getBytes());
-        System.out.print("\u001B[32m"+"cargado con exito");
-    }
-    static void decodificacion(String frase,int numero)throws IOException{
-        String abc = "abcdefghijklmnñopqrstuvwxyz";
+    public static void cifrar(int desplazamientoF,String textoF,String rutaEF,String rutaSF){
+        String abc = "abcdefghijkl mnñopqrstuvwxyz";
         char [] abcd = new char[abc.length()];
-        char [] cadena = new char[frase.length()];
-        int posicion = 0;//variable completamente inutil
-        for (int i = 0; i<frase.length();i++){//cargo la cadena con la frase, seguramente al pedo
-            cadena[i]=frase.charAt(i);
+        char [] cadena = new char[textoF.length()];
+        try {
+            File archivoF = new File(rutaEF);
+            // Si el archivo no existe es creado
+            if (!archivoF.exists()) {
+                archivoF.createNewFile();
+            }
+            FileWriter fw = new FileWriter(archivoF);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(textoF);
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i<textoF.length();i++){//cargo la cadena con la frase, seguramente al pedo
+            cadena[i]=textoF.charAt(i);
         }
         for (int j = 0; j<abc.length();j++){//cargo el array del abc tambien casi al pepe
             abcd[j]=abc.charAt(j);
         }
-        for (int o = 0; o< frase.length();o++){
+        for (int o = 0; o< textoF.length();o++){
             for (int p = 0; p< abc.length();p++){//recorro la frase caracter por caracter para desplazarlo
-                if (frase.charAt(o)==abc.charAt(p)){
-                    if ((numero+p)>abc.length()){//esto es de control, por si hay un caracter z
-                        int temp = (numero+p)-abc.length();
+                if (textoF.charAt(o)==abc.charAt(p)){
+                    if ((desplazamientoF+p)>abc.length()){//esto es de control, por si hay un caracter z
+                        int temp = (desplazamientoF+p)-abc.length();
                         cadena[o]=abcd[temp];
                     }
                     else {
-                        cadena[o]=abcd[p+numero];
+                        cadena[o]=abcd[p+desplazamientoF];
                     }
                 }
-                posicion++;
             }
-            posicion=o;
-            System.out.print(cadena[o]+",");
         }
-    }
-    static void escribo()throws IOException{
-        FileWriter fichero = null;
-        PrintWriter pw = null;
-        try
-        {
-            fichero = new FileWriter("c:/prueba.txt");
-            pw = new PrintWriter(fichero);
-
-            for (int i = 0; i < 10; i++)
-                pw.println("Linea " + i);
-
+        String contenidoC = new String(cadena);
+        System.out.println(contenidoC);
+        try {
+            File archivoF = new File(rutaSF);
+            // Si el archivo no existe es creado
+            if (!archivoF.exists()) {
+                archivoF.createNewFile();
+            }
+            FileWriter fw = new FileWriter(archivoF);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(contenidoC);
+            bw.close();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            try {
-                // Nuevamente aprovechamos el finally para
-                // asegurarnos que se cierra el fichero.
-                if (null != fichero)
-                    fichero.close();
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
         }
     }
-    public static void main(String[] args)throws IOException {
-        char modo = 'q';
-        String texto;
-        Scanner scan = new Scanner(System.in);
-        File archivo = null;
-        FileReader fr = null;
+    public static void descifrar(int desplazamientoF, String rutaSF) {
+        String abc = "abcdefghijkl mnñopqrstuvwxyz";
         BufferedReader br = null;
-        Path archivoE = Paths.get("C:\\Users\\danie\\IdeaProjects\\clase 4\\textoEntrada.txt");
-        Path archivoS = Paths.get("C:\\Users\\danie\\IdeaProjects\\clase 4\\textoSalida.txt");
         try {
-            archivo=new File("C:\\Users\\danie\\IdeaProjects\\clase 4\\textoEntrada.txt");
-            fr = new FileReader(archivo);
-            br = new BufferedReader(fr);
-        } catch (Exception e) {
+            br = new BufferedReader(new FileReader(rutaSF));
+            String texto = br.readLine();
+            while(texto == null) {
+                System.out.println(texto);
+                texto = br.readLine();
+            }
+            String[] contenido = new String[texto.length()];
+            for (int i = 0; i < texto.length();i++){
+                for (int p = 0; p < abc.length();p++){
+                    if (texto.charAt(i)==abc.charAt(p)){
+                        if ((p-desplazamientoF)<0){
+                            int temp= abc.length()+(p-desplazamientoF);
+                            contenido[i] = String.valueOf(abc.charAt(temp));
+                        } else{
+                            contenido[i]= String.valueOf(abc.charAt(p-desplazamientoF));
+                        }
+                    }
+                }
+            }
+            texto= new String(Arrays.toString(contenido));
+            File archivoF = new File(rutaSF);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(archivoF));
+            bw.write(texto);
+            bw.close();
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        do {
-            if (modo == 'c') {
-                System.out.print("\ningrese el texto: ");
-                texto = scan.next().toLowerCase(Locale.ROOT);
-                codificacion(texto, archivoE);
-            } else if (modo == 'd') {
+    }
 
+
+    public static void main(String ars[]) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+        String contenido="asdas";
+        String rutaE = "C:\\Users\\danie\\OneDrive\\Escritorio\\Argentina Programa\\Repositorio\\ArgentinaPrograma4.0\\IdeaProjects\\clase 4\\textoEntrada.txt";
+        String rutaS = "C:\\Users\\danie\\OneDrive\\Escritorio\\Argentina Programa\\Repositorio\\ArgentinaPrograma4.0\\IdeaProjects\\clase 4\\textoSalida.txt";
+        char modo;
+        int desplazamiento;
+        System.out.print("modo codificar 'c' o decodificar 'd' o 's'(salir): ");
+        modo = teclado.next().charAt(0);
+        while (modo!='s'){
+            if (modo=='c'){
+                System.out.print("desplazamiento : ");
+                desplazamiento= Integer.parseInt(teclado.next());
+                teclado.nextLine();
+                System.out.print("texto : ");
+                contenido = teclado.nextLine();
+                cifrar(desplazamiento,contenido,rutaE,rutaS);
+            } else if (modo=='d') {
+                System.out.print("desplazamiento : ");
+                desplazamiento= Integer.parseInt(teclado.next());
+                teclado.nextLine();
+                descifrar(desplazamiento,rutaS);
             }
-            System.out.print("\nCodificacion 'c' Decodificacion 'd' o salir 's': ");
-            modo = scan.next().charAt(0);
-        } while (modo != 's');
+            System.out.print("modo codificar 'c' o decodificar 'd' o 's'(salir): ");
+            modo = teclado.next().charAt(0);
+
+        }
     }
 }
